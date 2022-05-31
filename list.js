f5579100681807d3e46d2b1a203a1239b8845860
jjoki1258 <71545293+jjoki1258@users.noreply.github.com>
2022-05-31 15:27:29 +0900

    Update list.js

--- list.js
+++ list.js
@@ -5,7 +5,7 @@
     pageEncoding="EUC-KR"%>
 <%@ page import="java.*" %>	
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC >
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
